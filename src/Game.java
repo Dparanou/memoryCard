c95729a7a631e651cff00bea8340033f48ca4bc1
Dimitra Paranou <dimitra.paranou@student.uleige.com>
2018-10-31 18:55:17 +0200

    Add class getCoordinates

--- src/Game.java
+++ src/Game.java
@@ -9,13 +9,13 @@ public class Game {
 
 
   public Game (int x, int y, int times) {
-    b = new Board(x,y);
+    //b = new Board(x,y);
 
-    getCons();
+    getCoordinates();
 
   }
 
-  public void getCons() {
+  public int[] getCoordinates() {
       Scanner s = new Scanner(System.in);
 
       System.out.print("Δώσε την γραμμή και την στήλη της 1 κάρτας: ");
@@ -23,15 +23,19 @@ public class Game {
       System.out.println();
 
       System.out.print("Δώσε την γραμμή και την στήλη της 2 κάρτας: ");
-      String choice2= s.nextLine();
+      choice1 += " " + s.nextLine();
       System.out.println();
 
       String[] cons = choice1.split(" ");
-      //cons = choice2.split(" ");
 
-      for(int i = 0 ; i < cons.length ; i++) {
-        System.out.print(cons[i]);
+      int[] coord = new int[cons.length];
+
+      for (int i = 0 ; i < cons.length ; i++) {
+        coord[i] = Integer.parseInt(cons[i]);
+        System.out.println(coord[i]);
       }
+
+      return coord;
 
   }
 

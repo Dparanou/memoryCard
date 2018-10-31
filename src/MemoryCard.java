0c8ca0d62d13ce854657c3f0d3bc492ecbd67690
Dimitra Paranou <dimitra.paranou@student.uleige.com>
2018-10-31 17:45:36 +0200

    Change at MemoryCard

--- src/MemoryCard.java
+++ src/MemoryCard.java
@@ -4,16 +4,40 @@
  * and open the template in the editor.
  */
 package memorycard;
-/**
- *
- * @author dimitra
- */
+
+import java.util.Scanner;
+
 public class MemoryCard {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      System.out.println("test");
-        // TODO code application logic here
+      int i;
+      Scanner s = new Scanner(System.in);
+
+      do {
+        System.out.println("1.Βασικό");
+        System.out.println("2.Διπλάσιο");
+        System.out.println("3.Τρίο");
+        System.out.println("4.Έξοδος");
+
+        i = s.nextInt();
+
+        switch(i) {
+                case 1 : System.out.println("1!");
+                  break;
+                case 2 : System.out.println("2!");
+                  break;
+                case 3 : System.out.println("3!");
+                  break;
+                case 4 : System.out.println("Bye!");
+                  break;
+                default : System.out.println("Λάθος επιλογή!");
+                  break;
+      }
+
+      } while (i != 4 );
+
+
     }
 }
